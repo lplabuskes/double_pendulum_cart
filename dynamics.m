609fc8f2886f4146def22760cad20d306bd0de1c
lplabuskes <lplabuskes@gmail.com>
2021-06-30 17:27:42 -0400

    Minor corrections to system dynamics

--- dynamics.m
+++ dynamics.m
@@ -2,7 +2,7 @@ function [state_dot] = dynamics(state, u)
 %Compute the time evolution of a double pendulum on a cart with force u on
 %the cart
 %% Constants
-m_c = 1;
+m_c = 3;
 m_p = 1;
 l = 1;
 g = 9.81;
@@ -17,13 +17,13 @@ theta1dot = state(5);
 theta2dot = state(6);
 
 %equations derived from Lagrangian mechanics
-mass_matrix = [m_c+2*m_p, -1.5*m_p*l*cos(theta1), -.5*m_p*l*cos(theta2);
-               -1.5*m_p*l*cos(theta1), 1.25*m_p*l^2, .5*m*l^2*cos(theta1-theta2);
-               -.5*m_p*l*cos(theta2), .5*m*l^2*cos(theta1-theta2), .25*m_p*l^2];
+mass_matrix = [m_c+2*m_p, -2*m_p*l*cos(theta1), -m_p*l*cos(theta2);
+               -2*m_p*l*cos(theta1), 2*m_p*l^2, m_p*l^2*cos(theta1-theta2);
+               -m_p*l*cos(theta2), m_p*l^2*cos(theta1-theta2), m_p*l^2];
 
-rhs = [u - 1.5*m_p*l*theta1dot^2*sin(theta1) - .5*m_p*l*theta2dot^2*sin(theta2);
-       1.5*m_p*g*l*sin(theta1) - .5*m_p*l^2*theta2dot^2*sin(theta1-theta2);
-       .5*m_p*g*l*sin(theta2) + .5*m_p*l^2*theta1dot^2*sin(theta1-theta2)];
+rhs = [u - 2*m_p*l*theta1dot^2*sin(theta1) - m_p*l*theta2dot^2*sin(theta2);
+       2*m_p*g*l*sin(theta1) - m_p*l^2*theta2dot^2*sin(theta1-theta2);
+       m_p*g*l*sin(theta2) + m_p*l^2*theta1dot^2*sin(theta1-theta2)];
    
 state_dot(4:end) = mass_matrix\rhs;
 
