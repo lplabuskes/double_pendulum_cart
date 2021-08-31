9d8713da7413253911e06fae27e9e5f7ea5280db
Landon Labuskes <lplabuskes@gmail.com>
2021-08-31 16:57:00 -0400

    Port to Python

---
+++ dynamics.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+class CartSystem:
+    dt = .01
+
+    def __init__(self, initial_conditions):
+        self.state = initial_conditions
+
+    def system_dynamics(self, u):
+        # Constants
+        m_c = 3  # cart mass
+        m_p = 1  # pendulum mass
+        l_p = 1  # pendulum length
+        g = 9.81
+        c_c = .2  # cart damping coefficient
+        c_p = .1  # pendulum damping coefficient
+
+        # State Equations
+        theta1, theta2 = self.state[1], self.state[2]
+        theta1dot, theta2dot = self.state[4], self.state[5]
+
+        # Equations derived from Lagrangian mechanics
+        mass_matrix = np.array([[m_c+2*m_p, -2*m_p*l_p*np.cos(theta1), -m_p*l_p*np.cos(theta2)],
+                                [-2*m_p*l_p*np.cos(theta1), 2*m_p*l_p*l_p, m_p*l_p*l_p*np.cos(theta1-theta2)],
+                                [-m_p*l_p*np.cos(theta2), m_p*l_p*l_p*np.cos(theta1-theta2), m_p*l_p*l_p]])
+
+        rhs = np.array([[u - 2*m_p*l_p*(theta1dot**2)*np.sin(theta1) - m_p*l_p*(theta2dot**2)*np.sin(theta2)],
+                        [2*m_p*g*l_p*np.sin(theta1) - m_p*l_p*l_p*(theta2dot**2)*np.sin(theta1-theta2)],
+                        [m_p*g*l_p*np.sin(theta2) + m_p*l_p*l_p*(theta1dot**2)*np.sin(theta1-theta2)]])
+
+        second_derivs = np.linalg.solve(mass_matrix, rhs)
+
+        state_derivs = np.array([self.state[3], self.state[4], self.state[5], second_derivs[0][0], second_derivs[1][0], second_derivs[2][0]])
+
+        # Include damping
+        state_derivs[3:] -= np.array([c_c, c_p, c_p]).T * self.state[3:]
+
+        self.state += self.dt*state_derivs
---
+++ simulation.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+import dynamics
+
+x0 = np.array([0, .01, -.01, 0, 0, 0])
+cart = dynamics.CartSystem(x0)
+
+fig, axes = plt.subplots(1, 1)
+axes.set_ylim(-2, 4)
+axes.set_xlim(-3, 3)
+cart_line, = axes.plot([], [], 'k')
+pendulum_line, = axes.plot([], [], '.k-', markersize=12)
+plt.style.use("ggplot")
+
+
+def animation_function(frame, simulator):
+    simulator.system_dynamics(0)
+    state = simulator.state
+
+    width = .6
+    height = .4
+    length = 1
+    x_left, x_right = state[0] - width/2, state[0] + width/2
+
+    cart_x = np.array([x_left, x_right, x_right, x_left, x_left])
+    cart_y = np.array([0, 0, height, height, 0])
+
+    pendulum_x = state[0] + np.array([0, -length*np.sin(state[1]), -length*(np.sin(state[1]) + np.sin(state[2]))])
+    pendulum_y = height/2 + np.array([0, length*np.cos(state[1]), length*(np.cos(state[1]) + np.cos(state[2]))])
+
+    cart_line.set_xdata(cart_x)
+    cart_line.set_ydata(cart_y)
+
+    pendulum_line.set_xdata(pendulum_x)
+    pendulum_line.set_ydata(pendulum_y)
+
+
+anim = FuncAnimation(fig, animation_function, frames=100, fargs=(cart,), interval=10)
+plt.show()
